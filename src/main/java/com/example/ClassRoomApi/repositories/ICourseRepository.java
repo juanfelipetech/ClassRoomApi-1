0103169e23f53a33ba76d39ba3da930629459918
Christian Ramos <christianramosarias2@gmail.com>
2025-03-28 09:03:42 -0500

    feat se crea algunos repositorios

---
+++ src/main/java/com/example/ClassRoomApi/repositories/ICourseRepository.java
@@ -0,0 +1,4 @@
+package com.example.ClassRoomApi.repositories;
+
+public interface ICourseRepository {
+}
---
+++ src/main/java/com/example/ClassRoomApi/repositories/ITeacherRepository.java
@@ -0,0 +1,7 @@
+package com.example.ClassRoomApi.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITeacherRepository {
+}
