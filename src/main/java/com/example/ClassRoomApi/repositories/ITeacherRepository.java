2165f51ae65191213ff922903805820152a55018
Christian Ramos <christianramosarias2@gmail.com>
2025-03-28 09:10:53 -0500

    feat se crea @Repository en ITeacherRepository

--- src/main/java/com/example/ClassRoomApi/repositories/ITeacherRepository.java
+++ src/main/java/com/example/ClassRoomApi/repositories/ITeacherRepository.java
@@ -1,7 +1,9 @@
 package com.example.ClassRoomApi.repositories;
 
+import com.example.ClassRoomApi.models.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ITeacherRepository {
+public interface ITeacherRepository extends JpaRepository<Teacher,Integer> {
 }
