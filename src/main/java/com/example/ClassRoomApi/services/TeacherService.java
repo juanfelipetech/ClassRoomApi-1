c9e2dd427ee4e512a56abfb64802773773fd0ca9
Anselmo Ortega <lfkanselmo@gmail.com>
2025-04-04 10:08:07 -0500

    feat se crea servicio update

--- src/main/java/com/example/ClassRoomApi/services/TeacherService.java
+++ src/main/java/com/example/ClassRoomApi/services/TeacherService.java
@@ -5,6 +5,8 @@ import com.example.ClassRoomApi.repositories.ITeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TeacherService {
 
@@ -14,7 +16,7 @@ public class TeacherService {
     //En el servicio implementamos los métodos que necesitemos según los servicios a ofrecer
 
     //GUARDAR
-    public Teacher guardarTeacher(Teacher dataTeacher)throws Exception{
+    public Teacher saveTeacher(Teacher dataTeacher)throws Exception{
         try{
             return this.repository.save(dataTeacher);
         }catch (Exception mistake){
@@ -22,6 +24,24 @@ public class TeacherService {
         }
     }
     //MODIFICAR
+    public Teacher modifyTeacher(Integer id,Teacher dataTeacher)throws Exception{
+        try {
+            //Para modificar primero debo buscar
+            //JPA me devuelve un dato opcional (puede estar o no)
+            Optional<Teacher> searchedTeacher = this.repository.findById(id);
+            //Apenas lo busqués pregunta si si está
+            if(searchedTeacher.isPresent()){
+                //Modifiquelo pues papá
+                searchedTeacher.get().setExpertise(dataTeacher.getExpertise());
+                return this.repository.save(searchedTeacher.get());
+            }else{
+                //No estaba el muñeco
+                throw new Exception("El usuario a modificar no se encuetra en BD");
+            }
+        }catch (Exception mistake){
+            throw new Exception(mistake.getMessage());
+        }
+    }
 
     //BUSCAR POR ID
 
